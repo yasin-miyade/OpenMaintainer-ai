c6307665fd80ca4f23fd30ec90d8c6059f03fd45
Yasin Miyade <yasinmiyade66@gmail.com>
2026-09-19 20:47:58 +0530

    Main

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -20,6 +20,9 @@ export default function Home() {
             </code>{" "}
             file.
           </h1>
+          <h1 className="text-2xl font-semibold text-black bg-red-600 dark:text-zinc-50">
+            Testing
+          </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
